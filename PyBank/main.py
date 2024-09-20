8e3516017ea87770b29197f0271da97b12565588
Aidan David <132293345+AidanDavid@users.noreply.github.com>
2024-09-19 21:23:21 -0400

    Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,15 @@
 # -*- coding: UTF-8 -*-
-"""PyBank Homework Starter File."""
+"""PyBank Homework Starter File.
+Aidan David"""
 
-# Dependencies
 import csv
 import os
 
-# Files to load and output (update with correct file paths)
-file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
+# files to load and output
+file_to_load = os.path.join("Resources", "budget_data.csv")
+file_to_output = os.path.join("analysis", "budget_analysis.txt")
 
-# Define variables to track the financial data
+# initialize variables
 total_months = 0
 total_net = 0
 months = 1
@@ -18,7 +18,7 @@ curr_val = 0
 val_change = []
 val_total = 0
 
-# Open and process csv
+# open csv file
 with open(file_to_load) as financial_data:
     reader = csv.reader(financial_data)
 
@@ -29,7 +29,9 @@ with open(file_to_load) as financial_data:
     line = next(reader)
     max_val, min_val = (line[0], int(line[1])), (line[0], int(line[1]))
 
+    # loop through every row
     for row in reader:
+
         # get number of months
         months = months + 1
 
@@ -46,13 +48,10 @@ with open(file_to_load) as financial_data:
         if curr_val < min_val[1]:
             min_val = (row[0], curr_val)
 
-    output = "Financial Analysis" + "\n----------------------------" + "\nTotal Months: " + str(months) + "\nTotal: $" + str(val_total) + "\nAverage Change: $" + str(round(val_change/months, 2)) + "\nGreatest Increase in Profits: " + max_val[0] + " ($"+ str(max_val[1]) + ")" + "\nGreatest Decrease in Profits: " + min_val[0] + " ($"+ str(min_val[1]) + ")"
+    output = "Financial Analysis\n----------------------------\nTotal Months: " + str(months) + "\nTotal: $" + str(val_total) + "\nAverage Change: $" + str(round(val_change/months, 2)) + "\nGreatest Increase in Profits: " + max_val[0] + " ($"+ str(max_val[1]) + ")" + "\nGreatest Decrease in Profits: " + min_val[0] + " ($"+ str(min_val[1]) + ")"
 
+    # print output in terminal
     print(output)
-
-# clear text file
-#with open(file_to_output, "w") as txt_file:
-#    pass
 
 # write results to text file
 with open(file_to_output, "w") as txt_file:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,70 +1,46 @@
 # -*- coding: UTF-8 -*-
-"""PyPoll Homework Starter File."""
+"""PyPoll Homework Starter File.
+Aidan David"""
 
-# Import necessary modules
 import csv
 import os
 
-# Files to load and output (update with correct file paths)
-file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
-file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output file path
+# files to load and output
+file_to_load = os.path.join("Resources", "election_data.csv")
+file_to_output = os.path.join("analysis", "election_analysis.txt")
 
-# Initialize variables to track the election data
-total_votes = 0  # Track the total number of votes cast
+# initialize variables
+total_votes = 0
+votes_dict = {}
 
-# Define lists and dictionaries to track candidate names and vote counts
-
-
-# Winning Candidate and Winning Count Tracker
-
-
-# Open the CSV file and process it
+# open csv file
 with open(file_to_load) as election_data:
     reader = csv.reader(election_data)
 
-    # Skip the header row
+    # skip first line
     header = next(reader)
 
-    # Loop through each row of the dataset and process it
+    # loop through every row
     for row in reader:
 
-        # Print a loading indicator (for large datasets)
-        print(". ", end="")
+        # get total votes
+        total_votes = total_votes + 1
 
-        # Increment the total vote count for each row
+        votes_dict[row[2]] = votes_dict.get(row[2], 0) + 1
 
+    winner = max(votes_dict, key=votes_dict.get)
 
-        # Get the candidate's name from the row
+    # form output
+    output = "Election Results\n-------------------------\nTotal Votes: " + str(total_votes) + "\n-------------------------"
 
+    for name, votes in votes_dict.items():
+        output = output + "\n" + name + ": " + str(round(votes/total_votes*100, 3)) + "% (" + str(votes) + ")"
 
-        # If the candidate is not already in the candidate list, add them
+    output = output + "\n-------------------------" + "\n Winner: " + winner + "\n-------------------------"
 
+    # print output in terminal
+    print(output)
 
-        # Add a vote to the candidate's count
-
-
-# Open a text file to save the output
+# write results to text file
 with open(file_to_output, "w") as txt_file:
-
-    # Print the total vote count (to terminal)
-
-
-    # Write the total vote count to the text file
-
-
-    # Loop through the candidates to determine vote percentages and identify the winner
-
-
-        # Get the vote count and calculate the percentage
-
-
-        # Update the winning candidate if this one has more votes
-
-
-        # Print and save each candidate's vote count and percentage
-
-
-    # Generate and print the winning candidate summary
-
-
-    # Save the winning candidate summary to the text file
+    txt_file.write(output)
